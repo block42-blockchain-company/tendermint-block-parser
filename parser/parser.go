0c9c19d42f1191bb8a06714296be03cfe8cb0db3
DollyVolley <oliver.schwarz@block42.tech>
2021-03-09 16:29:43 +0100

    Change Environment variable

--- parser/parser.go
+++ parser/parser.go
@@ -46,8 +46,7 @@ func NewParser(endpoint *url.URL, timeout time.Duration) (*Parser, error) {
 	}
 
 	var mongoDbURI = "mongodb://thornode-bot-mongodb:27017/"
-	if os.Getenv("TM_CLIENT_DEV") != "" {
-		fmt.Printf("Running in development environment\n")
+	if os.Getenv("NATIVE_DEPLOYMENT") != "" {
 		mongoDbURI = "mongodb://localhost:27017/"
 	}
 
